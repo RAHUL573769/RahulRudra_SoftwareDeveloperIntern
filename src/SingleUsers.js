5a5132e5c45f1115c74e600dde6e3853b1c37875
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-10-09 12:50:15 +0600

    search implemented

--- src/SingleUsers.js
+++ src/SingleUsers.js
@@ -3,14 +3,16 @@ import "./single.css";
 const SingleUsers = ({ user }) => {
   const { name, username, email, phone } = user;
   return (
-    <div className="activity">
-      <h1>{name}</h1>
-      <h3>User Name:{username}</h3>
-      <p>
-        Email:{email}
-        <br />
-        Phone:{phone}
-      </p>
+    <div>
+      <div className="activity">
+        <h1>{name}</h1>
+        <h3>User Name:{username}</h3>
+        <p>
+          Email:{email}
+          <br />
+          Phone:{phone}
+        </p>
+      </div>
     </div>
   );
 };
--- src/Users.js
+++ src/Users.js
@@ -5,16 +5,36 @@ import "./Users.css";
 const Users = () => {
   const [users, setUsers] = useState([]);
 
+  const [searchResult, setSearchResult] = useState([]);
+
+  const handleSearchChange = (event) => {
+    const searchName = event.target.value;
+    const match = users.filter((n) => n.name.includes(searchName));
+    setSearchResult(match);
+  };
+
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((data) => setUsers(data));
   }, []);
   return (
-    <div className="container">
-      {users.map((user) => (
-        <SingleUsers key={user.id} user={user}></SingleUsers>
-      ))}
+    <div>
+      {console.log(users)}
+      <div style={{ margin: "20px", textAlign: "center" }}>
+        <input
+          onChange={handleSearchChange}
+          type="text"
+          id=""
+          placeholder="Enter Name"
+        />
+        <input type="text" name="" id="" placeholder="Enter Name" />
+      </div>
+      <div className="container">
+        {searchResult.map((user) => (
+          <SingleUsers key={user.id} user={user}></SingleUsers>
+        ))}
+      </div>
     </div>
   );
 };
